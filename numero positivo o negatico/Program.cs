b1a2ce29b9da8092121453880ab1bec34422644f
pcarcamo <paovargas810@gmail.com>
2016-08-12 15:56:06 -0600

    Numero positivo

---
+++ numero positivo o negatico/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numero_positivo_o_negatico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresa un numero");
+            int numero = Convert.ToInt16(Console.ReadLine());
+            { 
+            if (numero > 0)
+               Console.WriteLine("El numero " + numero + " es positivo");
+            else
+               Console.WriteLine("El numero " + numero + " es negativo");
+                }
+            Console.ReadLine();
+        }
+            
+     }
+ }
+
+
