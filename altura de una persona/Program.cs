60fa547338d58bb05a340482032a88c27a1d069e
pcarcamo <paovargas810@gmail.com>
2016-08-12 15:47:42 -0600

    Altura de una persona

---
+++ altura de una persona/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace altura_de_una_persona
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero; string altura;
+            Console.Write("INGRESE SU ALTURA :");
+            altura = Console.ReadLine();
+            numero = int.Parse(altura);
+            if (numero < 150)
+            {
+                Console.Write("SU ALTURA ES BAJA");
+            }
+            else
+            {
+                Console.Write("SU ALTURA ES PROMEDIO");
+            }
+            if (numero > 170)
+            {
+                Console.Write("SU ALTURA ES MAYOR A LA PROMEDIO");
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+
+
