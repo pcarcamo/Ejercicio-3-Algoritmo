b30857d1d257d6838fcfbf8459a84ec99a0f6f2f
pcarcamo <paovargas810@gmail.com>
2016-08-12 15:57:36 -0600

    Elección de voto

---
+++ Voto segun edad/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Voto_segun_edad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad;
+
+            string teclado;
+
+            Console.WriteLine("  Elegible Para votar ");
+             
+            Console.WriteLine("Ingresa su fecha de nacimiento : ");
+            teclado = Console.ReadLine();
+            edad = int.Parse(teclado);
+            if (edad <= 1998)
+            {
+                Console.Write("\n Felicidades puedes Votar: ");
+            }
+            else
+            {
+                Console.WriteLine("No puedes Votar: ");
+            }
+                     
+               Console.ReadLine();
+
+        }
+    }
+}
