49c264181a45f2a2a938c03d8d40461f74357987
pcarcamo <paovargas810@gmail.com>
2016-08-12 15:56:37 -0600

    Numero igual

---
+++ Numeros iguales o no/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numeros_iguales_o_no
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float num1;
+            float num2;
+            Console.WriteLine("Escribe el primer número");
+            num1 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Escribe el segundo número");
+            num2 = Convert.ToInt16(Console.ReadLine());
+            if (num1 == num2)
+                Console.WriteLine("Los numeros son iguales");
+            else
+                Console.WriteLine("Los numeros no son iguales" );
+            Console.ReadLine();
+        }
+    }
+
+}
+
